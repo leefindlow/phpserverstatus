9e9bc7a2a33423dc49c04628f6da63c425bff802
Lee Findlow <leefindlow@london.lan.findlow.org.uk>
2010-08-07 14:52:59 +0000

    Updated meta tags

--- index.php
+++ index.php
@@ -18,9 +18,20 @@ $cfg['services'][5]	=	array('name' => 'Telnet','port' => 23, 'description' => 'C
 		<title>PHP Server Status</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<style type="text/css" media="screen">@import "./style.css";</style>
-		<meta name="author" value="Lee Findlow <projects@leefindlow.com>">
-		<meta name="keywords" value="server,status,ping,server status,status checker,server checker">
-		<meta name="description" value="PHP Script to allow the checking of the status of a Server.">
+		<meta name="author" content="Lee Findlow <projects@leefindlow.com>">
+		<meta name="keywords" content="server,status,ping,server status,status checker,server checker">
+		<meta name="description" content="PHP Script to allow the checking of the status of a Server.">
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-7364000-1']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
 	</head>
 	<body>
 		<div class="wrapper">
