1f788a7d5f142823c1189318db6deb0ead795adf
Lee Findlow <leefindlow@london.lan.findlow.org.uk>
2010-08-07 15:12:02 +0000

    updated example code

--- example.php
+++ example.php
@@ -1,7 +1,6 @@
 <?php
 include('./ServerStatus.class.php');
 header('content-type:text/plain');
-print_r($_GET);
 //Server status test script
 $ss = new	ServerStatus();
 var_dump($ss->ServerStatus($_GET['hostname'],$_GET['port'], false));
